ae2e3d9e60df175d0b0ca2db8debb75726c0c159
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2026-04-25 17:05:01 +0600

    event listner controll

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div>
       <h1 className='text-5xl'>Hello World</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil velit dolor assumenda ipsum facere, doloremque deleniti ullam molestiae hic qui, asperiores corporis debitis cupiditate quidem, fugit esse accusamus explicabo autem?</p>
     </div>
   )
 }
