3bb09b9761a72e5a6d48e71ae78d680870fe3ba2
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2026-04-29 23:24:46 +0600

    AppContextProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Product from './pages/Product'
+import ProductDetails from './pages/ProductDetails'
+import Cart from './pages/Cart'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-5xl'>Hello World</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil velit dolor assumenda ipsum facere, doloremque deleniti ullam molestiae hic qui, asperiores corporis debitis cupiditate quidem, fugit esse accusamus explicabo autem?</p>
+    <div className=''>
+      <div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/product' element={<Product/>} />
+          <Route path='/product/:id' element={<ProductDetails/>} />
+          <Route path='/cart' element={<Cart/>} />
+        </Routes>
+      </div>
     </div>
   )
 }
---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+
+
+const AppContext = createContext(null)
+
+const AppContextProvider = ({children})=>{
+  return 
+    const value = {}
+    <AppContext.Provier value={value}>
+      {children}
+    </AppContext.Provier>
+  
+}
+
+export default AppContextProvider
